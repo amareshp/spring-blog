453b06cb31c63738245856ca80fc638397af4e0e
Amaresh Pattanaik <apattanaik@walmartlabs.com>
2015-05-18 16:23:27 -0700

    upload files - preserve original name

--- src/main/java/com/visitamaresh/ws/file/FileUploadController.java
+++ src/main/java/com/visitamaresh/ws/file/FileUploadController.java
@@ -3,9 +3,9 @@ package com.visitamaresh.ws.file;
 import java.io.BufferedOutputStream;
 import java.io.File;
 import java.io.FileOutputStream;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Controller;
@@ -30,17 +30,24 @@ public class FileUploadController {
         return "file/upload2";
     }
 
-    //@RequestParam("name") String name
+    //@RequestParam("file") MultipartFile file, @RequestParam("name") String name
     @RequestMapping(value="/upload", method=RequestMethod.POST)
-    public @ResponseBody String handleFileUpload(@RequestParam("file") MultipartFile file){
+    public @ResponseBody String handleFileUpload(@RequestParam("file") MultipartFile file, @RequestParam Map<String, Object> params) {
         String uploadFilePath = environment.getProperty("upload.path");
-        String name = file.getName();
-        
+        //String name = file.getName();
+        //MultipartFile file = (MultipartFile) params.get("file");
+        String name = file.getOriginalFilename();
+        String extn = null;
         if (!file.isEmpty()) {
             try {
+                int extensionIdx = name.lastIndexOf(".");
+                if(extensionIdx != -1) {
+                    extn = name.substring(extensionIdx);
+                    name = name.substring(0, extensionIdx);
+                }
                 byte[] bytes = file.getBytes();
-                String fileName = TimeUtil.getDateTimePart("yyyy-MM-dd-HH-mm-ss-SSS") + "-" + RandomUtil.randLong(5) + ".jpg";
-                //String filePath = uploadFilePath + fileName;
+                String fileName = name + "-" + TimeUtil.getDateTimePart("yyyy-MM-dd-HH-mm-ss-SSS") + extn;
+                //String filePath = uploadFilePath + name;
                 String filePath = uploadFilePath + fileName;
                 File yourFile = new File(filePath);
                 if(!yourFile.exists()) {
--- src/main/webapp/views/file/upload2.jsp
+++ src/main/webapp/views/file/upload2.jsp
@@ -5,18 +5,23 @@
 <script>
 function dzOptions() {
 	// "myAwesomeDropzone" is the camelized version of the HTML element's ID
-	nameOfFile = "abc";
-	Dropzone.options.myAwesomeDropzone = {
-	  paramName: file, // The name that will be used to transfer the file
+	Dropzone.options.dropzone = {
+	  paramName: "file", // The name that will be used to transfer the file
 	  uploadMultiple: true, 
 	  maxFilesize: 5, // MB
 	  accept: function(file, done) {
+		  
 	    if (file.name == "justinbieber.jpg") {
 	      done("Naha, you don't.");
 	    }
 	    else { done(); }
 	  }
 	};
+	
+	dropzone.on("drop", function(file, xhr, formData) {
+		  // Will send the filesize along with the file as POST data.
+		  formData.append("name", file.name);
+		});	
 }
 </script>
 </head>
@@ -28,7 +33,7 @@ function dzOptions() {
                  <h1 id="try-it-out">Try it out!</h1>
 
                 <div id="dropzone">
-                    <form action="/spring-blog/upload" class="dropzone dz-clickable" id="myAwesomeDropzone">
+                    <form action="/spring-blog/upload" class="dropzone dz-clickable" >
                         <div class="dz-message">Drop files here or click to upload.
                             <br> <span class="note">Selected files are <strong>/Users/apatta2/MyData/pics/upload</strong></span>
                         </div>
